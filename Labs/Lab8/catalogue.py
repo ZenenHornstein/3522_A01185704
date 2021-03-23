8e7af4903822d5b267c9300db231db5b759899c5
zenen hornstein <zenenhornstein2@gmail.com>
2021-03-22 20:42:11 -0700

    Initial commit

---
+++ Labs/Lab8/catalogue.py
@@ -0,0 +1,152 @@
+import difflib
+from Labs.Lab3.library_item_generator import LibraryItemGenerator
+import item
+
+
+class Catalogue:
+    """
+    Represents a catalogue that stores item of type Items.
+    """
+
+    def __init__(self, item_list):
+        """
+        Intialize the catalogue with a list of items.
+        :param item_list: a sequence of objects which subclass item.
+        """
+        self._item_list = item_list
+
+    def add_item(self):
+        item_to_add = LibraryItemGenerator.generate_item(classes=[item.Book, item.Journal, item.DVD])
+        self._item_list.append(item_to_add)
+
+    @property
+    def item_list(self):
+        """
+        The  item list of the Catalogue
+
+        :return : the item list of the Catalogue, as a list
+        """
+        return self._item_list
+
+    @item_list.setter
+    def item_list(self, new_item_list):
+        """
+        Set the item list of the Catalogue
+
+        :param new_item_list: the new item list of the Catalogue
+        :precondition :
+        """
+        self._item_list = new_item_list
+
+    def _retrieve_item_by_call_number(self, call_number):
+        """
+        A private method that encapsulates the retrieval of an book with
+        the given call number from the library.
+        :param call_number: a string
+        :return: book object if found, None otherwise
+        """
+        found_book = None
+        for library_book in self._item_list:
+            if library_book.call_number == call_number:
+                found_book = library_book
+                break
+        return found_book
+
+    def find_item(self, title):
+        """
+        Find books with the same and similar title.
+        :param title: a string
+        :return: a list of titles.
+        """
+        title_list = []
+        for library_book in self._item_list:
+            title_list.append(library_book.get_title())
+        results = difflib.get_close_matches(title, title_list,
+                                            cutoff=0.5)
+        return results
+
+    def remove_item(self, call_number):
+        """
+        Remove an existing book from the library
+        :param call_number: a string
+        :precondition call_number: a unique identifier
+        """
+        found_book = self._retrieve_item_by_call_number(call_number)
+        if found_book:
+            self._item_list.remove(found_book)
+            print(f"Successfully removed {found_book.get_title()} with "
+                  f"call number: {call_number}")
+        else:
+            print(f"book with call number: {call_number} not found.")
+
+    def display_available_items(self):
+        """
+        Display all the books in the library.
+        """
+        print("Books List")
+        print("--------------", end="\n\n")
+        for library_book in self._item_list:
+            print(library_book)
+
+    def reduce_item_count(self, call_number):
+        """
+        Decrement the book count for an book with the given call number
+        in the library.
+        :param call_number: a string
+        :precondition call_number: a unique identifier
+        :return: True if the book was found and count decremented, false
+        otherwise.
+        """
+        library_book = self._retrieve_item_by_call_number(call_number)
+        if library_book:
+            library_book.decrement_number_of_copies()
+            return True
+        else:
+            return False
+
+    def increment_item_count(self, call_number):
+        """
+        Increment the book count for an book with the given call number
+        in the library.
+        :param call_number: a string
+        :precondition call_number: a unique identifier
+        :return: True if the book was found and count incremented, false
+        otherwise.
+        """
+        library_book = self._retrieve_item_by_call_number(call_number)
+        if library_book:
+            library_book.increment_number_of_copies()
+            return True
+        else:
+            return False
+
+    def check_out(self, call_number):
+        """
+        Check out an book with the given call number from the library.
+        :param call_number: a string
+        :precondition call_number: a unique identifier
+        """
+        library_book = self._retrieve_item_by_call_number(call_number)
+        if library_book.check_availability():
+            status = self.reduce_item_count(call_number)
+            if status:
+                print("Checkout complete!")
+            else:
+                print(f"Could not find book with call number {call_number}"
+                      f". Checkout failed.")
+        else:
+            print(f"No copies left for call number {call_number}"
+                  f". Checkout failed.")
+
+    def return_item(self, call_number):
+        """
+        Return an book with the given call number from the library.
+        :param call_number: a string
+        :precondition call_number: a unique identifier
+        """
+        status = self.increment_item_count(call_number)
+        if status:
+            print("book returned successfully!")
+        else:
+            print(f"Could not find book with call number {call_number}"
+                  f". Return failed.")
---
+++ Labs/Lab8/library.py
@@ -0,0 +1,110 @@
+""" This module houses the library"""
+import item
+from catalogue import Catalogue
+import difflib
+
+class Library:
+    """
+    The Library consists of a list of books and provides an
+    interface for users to check out, return and find books.
+    """
+    def __init__(self, catalogue: Catalogue):
+        """
+        Intialize the library with a list of books.
+        :param book_list: a sequence of book objects.
+        """
+        self._catalogue = catalogue
+
+
+
+    def display_library_menu(self):
+        """
+        Display the library menu allowing the user to either access the
+        list of books, check out, return, find, add, remove a book.
+        """
+        user_input = None
+        while user_input != 7:
+            print("\nWelcome to the Library!")
+            print("-----------------------")
+            print("1. Display all Items")
+            print("2. Check Out an item")
+            print("3. Return an item")
+            print("4. Find an item")
+            print("5. Add an item")
+            print("6. Remove an item")
+            print("7. Quit")
+            string_input = input("Please enter your choice (1-7)")
+
+            #handle user pressing only enter in menu
+            if(string_input == ''):
+                continue
+
+            user_input = int(string_input)
+
+            if user_input == 1:
+                self._catalogue.display_available_items()
+               # self.display_available_books()
+                user_input = input("Press Enter to continue")
+            elif user_input == 2:
+                call_number = input("Enter the call number of the item"
+                                    " you wish to check out.")
+                self._catalogue.check_out(call_number)
+            elif user_input == 3:
+                call_number = input("Enter the call number of the item"
+                                    " you wish to return.")
+                self._catalogue.return_item(call_number)
+            elif user_input == 4:
+                input_title = input("Enter the title of the item:")
+                found_titles = self._catalogue.find_item(input_title)
+                print("We found the following:")
+                if len(found_titles) > 0:
+                    for title in found_titles:
+                        print(title)
+                else:
+                    print("Sorry! We found nothing with that title")
+
+            elif user_input == 5:
+                self._catalogue.add_item()
+
+            elif user_input == 6:
+                call_number = input("Enter the call number of the book")
+                self._catalogue.remove_item(call_number)
+
+            elif user_input == 7:
+                pass
+            else:
+                print("Could not process the input. Please enter a"
+                      " number from 1 - 7.")
+
+        print("Thank you for visiting the Library.")
+
+
+
+
+def generate_test_books():
+    """
+    Return a list of books with dummy data.
+    :return: a list
+    """
+    book_list = [
+        item.Book("100.200.300", "Harry Potter 1", 2, "J K Rowling"),
+        item.Book("999.224.854", "Harry Potter 2", 5, "J K Rowling"),
+        item.Book("631.495.302", "Harry Potter 3", 4, "J K Rowling"),
+        item.Book("123.02.204", "The Cat in the Hat", 1, "Dr. Seuss"),
+    ]
+
+    return book_list
+
+
+def main():
+    """
+    Creates a library with dummy data and prompts the user for input.
+    """
+    book_list = generate_test_books()
+    my_epic_catalogue = Catalogue(book_list)
+    my_epic_library = Library(my_epic_catalogue)
+    my_epic_library.display_library_menu()
+
+
+if __name__ == '__main__':
+    main()
